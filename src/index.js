afa03108c510e421526319461788adb4f90f918e
alaricode <antonlarichev@gmail.com>
2021-06-04 19:29:12 +0300

    Вложенная папка

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const { writeFileSync, readFileSync } = require('fs');
+const fse = require('fs-extra')
 
 const app = express()
 const port = 3000
-app.get('/set', (request, response) => {
-	writeFileSync('data', request.query.id);
+app.get('/set', async (request, response) => {
+	await fse.ensureDir('data');
+	writeFileSync('./data/req', request.query.id);
 	response.send('done!');
 })
 app.get('/get', (request, response) => {
-	const res = readFileSync('data');
+	const res = readFileSync('./data/req');
 	response.send(res.toString());
 })
 app.listen(port, (err) => {
